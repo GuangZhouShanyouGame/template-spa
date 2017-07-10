7ed68fcfd4f93de1ad7a06d0010016f7458af7ce
Yifeng <f15gdsy@gmail.com>
2017-07-10 11:01:08 +0800

    updated

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -18,10 +18,9 @@ module.exports = {
     ],
     // add your custom rules here
     'rules': {
-        'arrow-parens': 0,
-        'generator-star-spacing': 0,
-        'indent': [2, 4],
+        'arrow-parens': [2, 'as-needed'],   // 箭头函数参数只有一个时，可以不带括号
+        'indent': [2, 4, { 'SwitchCase': 1 }],   // 缩进为4个空格，Switch 的 Case 需要一个缩进
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'space-before-function-paren': [2, 'never']
+        'space-before-function-paren': [2, 'never']     // 函数参数括号前不需要空格
     }
 }
